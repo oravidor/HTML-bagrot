ed68a924bb83ac2034a890f2c08e9429db481004
oravidor <110723602+oravidor@users.noreply.github.com>
2024-11-18 08:03:39 +0200

    bug fixes

--- Bagrot HTML/Bagrot HTML/obj/Debug/net8.0/Bagrot HTML.AssemblyInfo.cs
+++ Bagrot HTML/Bagrot HTML/obj/Debug/net8.0/Bagrot HTML.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bagrot HTML")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f17c6a053fe2439ff240bb814d49535f4192d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c83e62f2cc8801fb99554e8a8595b05a2e557b6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bagrot HTML")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bagrot HTML")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
